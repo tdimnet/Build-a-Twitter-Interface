768d7b5e8434d788db4fdc90ba8d7dd8ac12f2f2
tdimnet <thomas.dimnet@gmail.com>
2017-05-30 13:08:08 +0200

    Refactored the config.js file and tested it

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 'use strict';
 
 const express   = require('express');
+const Twit      = require('twit');
 const config    = require('./config.js');
+
+const twit = new Twit(config);
 
 const app = express();
 
@@ -9,6 +12,12 @@ app.use(
   '/static',
   express.static(__dirname + '/public')
 );
+
+function test() {
+  twit.get('https://api.twitter.com/1.1/search/tweets.json?q=%23superbowl&result_type=recent', function(err, data, response) {
+    console.log(data)
+  })
+}
 
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/templates');
@@ -19,4 +28,5 @@ app.get('/', (request, response) => {
 
 app.listen(3000, () => {
   console.log('The server is running on port 3000');
+  test();
 });
