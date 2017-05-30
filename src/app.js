bda24401948f17451c4809239e6d05bf17aa0383
tdimnet <thomas.dimnet@gmail.com>
2017-05-30 13:54:56 +0200

    Fetch the data which are coming from the api

--- src/app.js
+++ src/app.js
@@ -13,10 +13,17 @@ app.use(
   express.static(__dirname + '/public')
 );
 
-function test() {
+function getFriendsList() {
   twit.get('https://api.twitter.com/1.1/followers/list.json?user_id=' + config.access_token + '', (err, data, res) => {
+    console.log(data.users.length);
+  })
+}
+
+
+//https://dev.twitter.com/rest/reference/get/users/show => There is an error => I can not access the data with the only access token..
+function getImageProfil() {
+  twit.get('https://api.twitter.com/1.1/users/show.json?user_id=713045356720873472', (err, data, res) => {
     console.log(data);
-    // console.log(config.access_token)
   })
 }
 
@@ -29,5 +36,6 @@ app.get('/', (request, response) => {
 
 app.listen(3000, () => {
   console.log('The server is running on port 3000');
-  test();
+  // getFriendsList();
+  getImageProfil();
 });
