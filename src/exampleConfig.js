70f285fb567cf69f4fd4c0b988b6ce2f6565847b
tdimnet <thomas.dimnet@gmail.com>
2017-07-16 19:44:54 +0200

    Add config example file

---
+++ src/exampleConfig.js
@@ -0,0 +1,9 @@
+const twitterAPIaccess = {
+    consumer_key:         '...',
+    consumer_secret:      '...',
+    access_token:         '...',
+    access_token_secret:  '...'
+}
+
+// export the module and import it into your js file
+module.exports = twitterAPIaccess;
