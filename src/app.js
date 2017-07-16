b416abf67230dc8d17b98f5ce72d5322529daf4f
tdimnet <thomas.dimnet@gmail.com>
2017-07-16 19:34:15 +0200

    Add twit module and start displaying info within the console

--- src/app.js
+++ src/app.js
@@ -2,16 +2,29 @@
 
 // Import statements
 const express = require('express');
+const Twit = require('twit');
+const config = require('./config.js');
 
 const app = express();
+const twitter = new Twit(config);
 
+// setter statements in order to show the templating files
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 
+const profilInfo = function getProfilInfo() {
+  twitter.get('search/tweets', { q: 'banana since:2011-07-11', count: 100 }, function(err, data, response) {
+    console.log(data)
+  });
+}
+
+// Match the home route
 app.get('/', (req, res) => {
     res.render('index');
+    profilInfo();
 });
 
+// Start server and watch for changes
 app.listen(3000, (
     console.log('THe server is now running on port 3000')
 ));
