76620a9a666bead1b027cfc29ef1a333a5d1b8ee
tdimnet <thomas.dimnet@gmail.com>
2017-07-22 16:07:24 +0200

    Add a new node library to deal with async calls. Retrieve data from verify credentials and followers list

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 
 // Import statements
 const express = require('express');
+const async = require('async');
 const Twit = require('twit');
 const config = require('./config.js');
 
@@ -21,9 +22,10 @@ app.set('views', __dirname + '/views');
 
 
 const profilInfo = function getProfilInfo() {
-  twitter.get('account/verify_credentials', (err, data, response) => {
-    // console.log(data);
-  });
+  const foo = twitter.get('account/verify_credentials', function(err, data, response) {
+    return data;
+  })
+  return foo;
 }
 
 const recentTweets = function getRecentTweets() {
@@ -46,23 +48,33 @@ const privateMessages = function getPrivateRecentMessages() {
 
 
 // Match the home route
-app.get('/', (req, res, next) => {
+app.get('/', (req, res) => {
     var twitData = {};
-    // For now, the first promise comes from here :)
-    twitter.get('account/verify_credentials', { skip_status: true })
-      .catch(err => {
-        console.log('caught error', err.stack)
-      })
-      .then(result => result.data)
-      .then(data => {
-        return twitData = {
-          username: data.screen_name
+
+    async.parallel(
+      [
+        function(next) {
+          twitter.get('account/verify_credentials', function(err, data, response) {
+            const firstData = data;
+            next(null, firstData)
+          })
+        },
+        function(next) {
+          twitter.get('followers/list', { count: 5 }, (err, data, response) => {
+            const secondData = data;
+            next(null, secondData)
+          });
         }
-      })
-      .then(newData => {
-        console.log(newData);
-        res.render('index', { twitterData: newData });
-      })
+      ], function(err, results) {
+        // Accessing followers object this way = results[1].users[0]
+        console.log(results[1].users[0]);
+        res.render('index');
+      }
+    )
+
+
+
+
   }
 );
 
